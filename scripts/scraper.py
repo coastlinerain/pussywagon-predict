3652daaf670141c23274cefb313e4cbfb729f175
elarreap <elarreap@external.unav.es>
2026-04-30 10:20:22 +0200

    v1.11

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -29,14 +29,14 @@ async def main():
         )
         page = await context.new_page()
 
-        for i in range(295, NUM_PAGINAS + 1):
+        for i in range(396, NUM_PAGINAS + 1):
             url = f"https://www.coches.net/segunda-mano/?PriceMax=15000&pg={i}"
             print(f"\n--- Extrayendo Página {i}: {url} ---")
             
             try:
                 await page.goto(url, wait_until="load", timeout=60000)
                 
-                if i == 295:
+                if i == 396:
                     try:
                         await page.click("button#didomi-notice-agree-button", timeout=5000)
                         print("Cookies aceptadas.")
